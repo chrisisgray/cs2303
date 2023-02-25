a69bb8f72855ddd68194909b81f52208ee8dd185
Cristobal Rincon Rogers <chrisrincon23@gmail.com>
2023-02-24 20:06:10 -0500

    question four complete! now on to question 5

--- hws/hw5/stacktest.c
+++ hws/hw5/stacktest.c
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include "mystring.h"
 
 /** main function to demonstrate use of stack
  *
@@ -18,28 +22,50 @@ int main (int argc, char *argv[]) {
 
   // Create a stack to hold our test data
   the_stack = create(size);  // creates stack with a size of inputed value
- int test_data[] = {1, 2, 3, 4, 5};
+
+  char *buffer = NULL;
+  size_t sz_buffer = 0;
+  int numChars = 0;
+
+  char prompt[] = "Type a string or <Ctrl-d> to end: ";
+
+  while(1) {
+    fprintf(stdout, "%s", prompt);
+    fflush(stdout);
+    if((numChars = getline(&buffer, 
+      &sz_buffer, stdin)) == EOF) {
+        break;
+      }
+
+      char *ptr = mystrdup(buffer);
+
+      push(the_stack, (void *) ptr);
+      //fprintf(stdout, "top of stack now %s \n", (char *) (peek(the_stack))); // casting void * and dereferencing it
+      //echo line back to user
+      //fprintf(stdout, "You typed: %s\n", buffer);
+      //fflush(stdout);
+
+      // release memory for next loop iteration
+      free(buffer);
+      buffer = NULL;
+      sz_buffer = 0;
+  } // end of while statement
 
 
-  printf ("sizeof(test_data)) == %ld\n", sizeof(test_data)); // size of test_data. it could be that I am not allocating
-                                                            // enough memory for it
-  
-  // Push some test data onto the stack
-
-  printf("%d\n", sizeof(int*));
-
-  for (int i = 0; i < 5; i++) {
-
-    push(the_stack, (void*) &test_data[i]);
-    printf("i is %d\n", i);
-
-#ifdef DEBUG_TEST
-    printf("pushing element \"%d\"\n", test_data[i]);
-
-    int *val = (int *) (peek(the_stack));
-    printf("top of stack now \"%d\"\n", *val); // casting void * and dereferencing it
-#endif
+// free up any memory
+if(buffer != NULL) {
+  free(buffer);
 }
+printf("num_elements: %d \n", the_stack->num_elements);
+
+int k = the_stack->num_elements;
+
+for (int i = 0; i < k; i++) {
+  printf("This is the value of i: %d \n", i);
+  printf("top of stack now %s \n", (char *)(pop(the_stack))); // casting void * and dereferencing it
+}
+
+  destroy(the_stack);
 
 return 0;
 
